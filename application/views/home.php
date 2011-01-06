c97f4c0505e43a52bfae77eae92f4af812725f5f
The Muffin <The Muffin@12712c27-1255-564a-9dae-badfe56b58dc>
2011-01-06 20:11:13 +0000

    changed version to 1.1
    git-svn-id: file:///home/dustin/repo/trunk@32 12712c27-1255-564a-9dae-badfe56b58dc

--- application/views/home.php
+++ application/views/home.php
@@ -1,5 +1,5 @@
 <h1>Select a category to begin</h1>
-<p class="info">The Campus Bullet v1.0 Beta. (C)2010 Dustin Masters.<br />
+<p class="info">The Campus Bullet v1.1 Beta. (C)2010 Dustin Masters.<br />
 Please report ANY bugs with the system, and make suggestions as well using the same tool :)</p>
 <div id="categories">
 <? foreach($categories as $category) { ?>
