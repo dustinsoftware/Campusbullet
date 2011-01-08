ace38c071538c63d1e07e1967c088205cc48759e
The Muffin <The Muffin@12712c27-1255-564a-9dae-badfe56b58dc>
2011-01-08 06:40:16 +0000

    git-svn-id: file:///home/dustin/repo/trunk@38 12712c27-1255-564a-9dae-badfe56b58dc

--- application/classes/controller/register.php
+++ application/classes/controller/register.php
@@ -19,7 +19,7 @@ class Controller_Register extends Controller_Layout {
 		}
 		
 		$content = View::factory('register_home');
-		$content->errors = array("Sorry, The Campus Bullet is currently beta testing and registrations are not allowed yet.&nbsp; Go sign up for a beta account <a href=\"http://beta.dustinsoftware.com/masterlist-beta\">here</a>!");
+		$content->errors = array();
 		
 		if ($_POST && empty($content->errors)) {
 			$errors = array();
