d2244d23eb0ab8f6c9d9b27b3940b1855e4f0e3d
dustin <dustin@12712c27-1255-564a-9dae-badfe56b58dc>
2011-08-07 13:48:33 +0000

    Changed version to 2.0
    git-svn-id: file:///home/dustin/repo/trunk@107 12712c27-1255-564a-9dae-badfe56b58dc

--- application/config/masterlist.php
+++ application/config/masterlist.php
@@ -2,7 +2,7 @@
 
 return array(
 	'root' => DOCROOT,
-	'version' => "1.8",
+	'version' => "2.0",
 	'curl_key' => 'kylerocks12345679',
 	'image_limit' => 4,
 	'image_filepath' => DOCROOT . "/images/posts",
