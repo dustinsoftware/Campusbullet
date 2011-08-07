4ffd0cf152ba9c6f9b53b77b1756242fbd4b1d7b
dustin <dustin@12712c27-1255-564a-9dae-badfe56b58dc>
2011-08-07 08:42:19 +0000

    Added image in case image can't be found
    git-svn-id: file:///home/dustin/repo/trunk@105 12712c27-1255-564a-9dae-badfe56b58dc

--- images/posts/index.php
+++ images/posts/index.php
@@ -5,14 +5,15 @@ if ($file == "") {
 	header('Location: /');
 } else {
 
-$file = preg_replace("/[^0-9\-]/", "", $file) . ".jpg";
+	$file = preg_replace("/[^0-9\-]/", "", $file) . ".jpg";
 
-if (file_exists($file)) {
-	 header('Content-Type: image/jpeg');
-	echo file_get_contents($file);
-} else {
-	header("HTTP/1.0 404 Not Found");
-	echo "Hey, jerkface. That image doesn't exist.";
-}
+	header('Content-Type: image/jpeg');
+	if (file_exists($file)) {
+		echo file_get_contents($file);
+	} else {
+		header("HTTP/1.0 404 Not Found");
+		header('Content-Type: image/jpeg');
+		echo file_get_contents("noimage.jpg");
+	}
 
 }
