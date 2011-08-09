81c0bd4e089ef72dce37c6802edacf682801bd95
dustin <dustin@12712c27-1255-564a-9dae-badfe56b58dc>
2011-08-09 17:17:38 +0000

    Forgot to add powersearch halfredirect view
    git-svn-id: file:///home/dustin/repo/trunk@114 12712c27-1255-564a-9dae-badfe56b58dc

---
+++ application/views/powersearch_halfredirect.php
@@ -0,0 +1,6 @@
+<h1>Half.com Outbound Page</h1>
+
+<p>Unfortunately, Half.com is dumb when it comes to displaying their website inside of ours.&nbsp;
+You must click the link below to open their site in a new window.</p>
+
+<h2 style="text-align: center"><a href="http://search.half.ebay.com/<?=@($_GET['isbn'])?>" target="_blank">Open Half.com in a separate window</a></h2>
