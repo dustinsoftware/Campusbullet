dcfce7952d5fed224dbaa5b12f1d14e5b7681b01
dustin <dustin@dustinsoftware.com>
2012-04-10 13:09:59 -0500

    Removed comment box

--- application/views/help_home.php
+++ application/views/help_home.php
@@ -17,14 +17,8 @@
 	<li><a href="<?=$url_base?>contact/message/ml_bug_report">Report a bug</a></li>
 	<li><a href="<?=$url_base?>contact/message/ml_abuse">Report abuse</a></li>
 </ul>
-<p>...or, write Dustin directly at <a href="mailto:dustin@campusbullet.net">
-dustin@campusbullet.net</a>.</p>
 
 </td>
 </tr>
 </table>
 
-<div class="commentsbox" >
-<div id="fb-root"></div><script src="http://connect.facebook.net/en_US/all.js#appId=195850837003&amp;xfbml=1"></script><fb:comments href="http://campusbullet.net/help/suggestions" num_posts="10" width="700"></fb:comments>
-
-</div>
